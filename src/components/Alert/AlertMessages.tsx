a1f3752b491e3e211eb2d63f06ec586c38b6622a
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2024-06-08 21:26:03 +0300

    Second commit

---
+++ src/components/Alert/AlertMessages.tsx
@@ -0,0 +1,14 @@
+interface IAlert {
+    title:string,
+    description:string
+}
+
+const Alert =({}:IAlert)=>{
+    return(
+        <>
+            <div>Alert</div>
+        </>
+    );
+};
+
+export default Alert;
